ecb772534f92c92bef33ea6a2bf802cf89391305
xiangyu000 <xiangyu000wei@yeah.net>
2022-03-01 15:51:30 +0800

    version1.2

---
+++ public/main.js
@@ -0,0 +1,96 @@
+let n = 1;
+getcss.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    console.log(request.response);
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      }
+    } else {
+      alert("fail");
+    }
+  };
+  request.send();
+};
+
+gethtml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onload = () => {
+    const div = document.createElement("div");
+    div.innerHTML = request.response;
+    document.body.appendChild(div);
+  };
+  request.onerror = () => {
+    console.log("fail");
+  };
+  request.send();
+};
+
+getjs.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onload = () => {
+    console.log(request.response);
+    const script = document.createElement("script");
+    script.innerHTML = request.response;
+    document.body.appendChild(script);
+  };
+  request.onerror = () => {
+    console.log("fail");
+  };
+  request.send();
+};
+
+getxml.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status < 300 && request.status >= 200) {
+        const dom = request.responseXML;
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text.trim());
+      }
+    }
+  };
+
+  request.send();
+};
+getjson.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status === 200) {
+      console.log(request.response);
+      const bool = JSON.parse(request.response);
+      console.log(typeof bool);
+      console.log(bool);
+    }
+  };
+  request.send();
+};
+getpage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}.json`);
+  request.onreadystatechange = () => {
+    if (
+      request.readyState === 4 &&
+      request.status >= 200 &&
+      request.status < 300
+    ) {
+      const array = JSON.parse(request.response);
+      array.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.id;
+        xxx.appendChild(li);
+      });
+      n += 1;
+    }
+  };
+  request.send();
+};
